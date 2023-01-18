0c70da4a9d6b00abd1814660d4207cf1f924bac0
Mohit27mk <114071726+Mohit27mk@users.noreply.github.com>
2023-01-18 20:09:34 +0530

    Classcommit

--- dom.js
+++ dom.js
@@ -6,3 +6,9 @@ var addItem =document.getElementById('add-item');
 addItem.style.fontWeight='bold';
 addItem.style.color='green';
 
+var items =document.getElementsByClassName('list-group-item');
+items[2].style.backgroundColor='green';
+
+for(var i=0;i<items.length;i++){
+    items[i].style.fontWeight='bold';
+}
