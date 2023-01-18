6dcc83f803a6d715d1a83089882b04d5901e220e
Mohit27mk <114071726+Mohit27mk@users.noreply.github.com>
2023-01-18 23:38:23 +0530

    Dom

--- dom.js
+++ dom.js
@@ -25,12 +25,28 @@
 // var thirdItem =document.querySelector('.list-group-item:nth-child(3)');
 // thirdItem.style.visibility="hidden"
 
-var odd = document.querySelectorAll('li:nth-child(odd)');
+// var odd = document.querySelectorAll('li:nth-child(odd)');
 
-for(var i=0;i<odd.length;i++){
-    odd[i].style.backgroundColor='green';
-}
+// for(var i=0;i<odd.length;i++){
+//     odd[i].style.backgroundColor='green';
+// }
 
-var secondItem=document.querySelectorAll('.list-group-item');
+// var secondItem=document.querySelectorAll('.list-group-item');
 
-secondItem[1].style.color='green';
+// secondItem[1].style.color='green';
+
+var itemList =document.querySelector('#items');
+// itemList.parentNode.style.backgroundColor='blue';
+
+var helloText=document.createTextNode('Hello World');
+
+var container=document.querySelector('header .container');
+var h1=document.querySelector('header h1');
+
+container.insertBefore(helloText,h1);
+
+var li=document.querySelector('li:nth-child(1)');
+console.log(li);
+
+itemList.insertBefore(helloText,li);
+
