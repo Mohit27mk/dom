78668aabf682f3235780ca1d19b1557f2552a6ef
Mohit27mk <114071726+Mohit27mk@users.noreply.github.com>
2023-01-18 15:30:49 +0530

    first commit

---
+++ dom.js
@@ -0,0 +1,8 @@
+var header =document.getElementById('main-header');
+header.style.border='solid 5px black';
+
+var addItem =document.getElementById('add-item');
+
+addItem.style.fontWeight='bold';
+addItem.style.color='green';
+
