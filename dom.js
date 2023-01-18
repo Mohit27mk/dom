10c290de3ee44a609020fd1a510a8b7dd4b216c9
Mohit27mk <114071726+Mohit27mk@users.noreply.github.com>
2023-01-18 22:32:13 +0530

    querySelector

--- dom.js
+++ dom.js
@@ -13,10 +13,24 @@
 //     items[i].style.fontWeight='bold';
 // }
 
-var li =document.getElementsByTagName('li');
-li[2].style.backgroundColor='green';
+// var li =document.getElementsByTagName('li');
+// li[2].style.backgroundColor='green';
 
-for(var i=0;i<li.length;i++){
-    li[i].style.color='purple';
+// for(var i=0;i<li.length;i++){
+//     li[i].style.color='purple';
+// }
+
+// var secondItem =document.querySelector('.list-group-item:nth-child(2)');
+// secondItem.style.backgroundColor='green';
+// var thirdItem =document.querySelector('.list-group-item:nth-child(3)');
+// thirdItem.style.visibility="hidden"
+
+var odd = document.querySelectorAll('li:nth-child(odd)');
+
+for(var i=0;i<odd.length;i++){
+    odd[i].style.backgroundColor='green';
 }
 
+var secondItem=document.querySelectorAll('.list-group-item');
+
+secondItem[1].style.color='green';
